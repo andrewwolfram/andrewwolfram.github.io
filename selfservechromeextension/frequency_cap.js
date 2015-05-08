8bed02d0283aef7ccb57c81e710387c91fa103a2
Andrew Wolfram <ajwolfram@gmail.com>
2015-05-08 11:58:39 -0400

    Update frequency cap js

--- selfservechromeextension/frequency_cap.js
+++ selfservechromeextension/frequency_cap.js
@@ -1,4 +1,4 @@
-document.getElementById("im_script").addEventListener.("load", function(){
+document.getElementById("im_script").addEventListener("load", function(){
     IntentMedia.ExitUnitOpener.options.is_frequency_capped = function() { return false; };
     IntentMedia.ExitUnitOpener.is_cross_site_eu_frequency_capped = function() { return false; };
     IntentMedia.ExitUnitOpener.opener_is_deactivated = function() { return false; };
