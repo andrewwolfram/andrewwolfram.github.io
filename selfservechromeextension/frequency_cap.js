3cf6efa697a45b79f1f42556f17b8a67b71f5b94
Andrew Wolfram <ajwolfram@gmail.com>
2015-05-08 17:10:46 -0400

    Update setInterval

--- selfservechromeextension/frequency_cap.js
+++ selfservechromeextension/frequency_cap.js
@@ -1,5 +1,5 @@
 var freqInterval = setInterval(function(){
-    if(IntentMedia.ExitUnitOpener.options != undefined) {
+    if(IntentMedia != undefined && IntentMedia.ExitUnitOpener != undefined && IntentMedia.ExitUnitOpener.options != undefined) {
     IntentMedia.ExitUnitOpener.options.is_frequency_capped = function() { return false; };
     IntentMedia.ExitUnitOpener.is_cross_site_eu_frequency_capped = function() { return false; };
     IntentMedia.ExitUnitOpener.opener_is_deactivated = function() { return false; };
