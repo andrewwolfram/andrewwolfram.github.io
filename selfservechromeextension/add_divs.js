66270e67cab628e15f3fb018f229471d3e685761
Andrew Wolfram <ajwolfram@gmail.com>
2015-05-08 16:50:41 -0400

    Fix freq cap override

--- selfservechromeextension/add_divs.js
+++ selfservechromeextension/add_divs.js
@@ -15,9 +15,9 @@ function addDivs() {
         return;
 
     var myExitUnitOpener = IntentMedia.ExitUnitOpener;
-    IntentMedia.ExitUnitOpener.options.is_frequency_capped = function() { return false; };
-    IntentMedia.ExitUnitOpener.is_cross_site_eu_frequency_capped = function() { return false; };
-    IntentMedia.ExitUnitOpener.opener_is_deactivated = function() { return false; };
+    IntentMedia.ExitUnitOpener.options.is_frequency_capped = function() { return true; };
+    IntentMedia.ExitUnitOpener.is_cross_site_eu_frequency_capped = function() { return true; };
+    IntentMedia.ExitUnitOpener.opener_is_deactivated = function() { return true; };
 
     var myCSS = document.createElement("link");
     myCSS.href = '//andrewwolfram.github.io/selfservechromeextension/on_page.css';
