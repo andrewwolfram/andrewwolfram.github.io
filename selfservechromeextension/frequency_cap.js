59c19536ba7cdfa97a52058d4cdb6cd51aa905af
Andrew Wolfram <ajwolfram@gmail.com>
2015-05-08 12:00:41 -0400

    Change frequency cap to simply use a timeout

--- selfservechromeextension/frequency_cap.js
+++ selfservechromeextension/frequency_cap.js
@@ -1,5 +1,5 @@
-document.getElementById("im_script").addEventListener("load", function(){
+setTimeout(function(){
     IntentMedia.ExitUnitOpener.options.is_frequency_capped = function() { return false; };
     IntentMedia.ExitUnitOpener.is_cross_site_eu_frequency_capped = function() { return false; };
     IntentMedia.ExitUnitOpener.opener_is_deactivated = function() { return false; };
-});
+}, 1000);
