21fd950f2ceaee89531d828dda9606e40a2fda04
Andrew Wolfram <ajwolfram@gmail.com>
2015-11-05 13:14:52 -0500

    Change position of iframes

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", carousel);
 
 function carousel(){
    console.log("working");
+   var carousel1 = document.getElementById("carousel1");
+   var carousel2 = document.getElementById("carousel2");
+   var carousel3 = document.getElementById("carousel3");
 } 
