e34556cb7d997d0ad5a2cf768ae71d7f9a59ea73
Andrew Wolfram <ajwolfram@gmail.com>
2016-07-24 23:32:12 -0400

    AW: Fix openWindow attempt

--- sw/sw.js
+++ sw/sw.js
@@ -8,9 +8,6 @@ self.addEventListener('activate', function(event) {
 });
 self.addEventListener('fetch', function(event) {
     console.log('Fetch event received', event);
-    //console.log('Attempting to open window');
-    console.log('clients!', clients);
-    console.log('client!', client);
-    console.log('WindowClient!', WindowClient);
-        //Clients.openWindow('//www.cnn.com');
+    console.log('Clients!', Clients.__proto__);
+    Clients.__proto__.openWindow('//www.cnn.com');
 });
