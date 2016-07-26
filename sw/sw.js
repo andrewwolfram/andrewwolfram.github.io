25e331f4857092a871b61a6bf4f8f64196dfeba2
Andrew Wolfram <ajwolfram@gmail.com>
2016-07-26 17:59:27 -0400

    AW: Attempt window focus on notificationclick

--- sw/sw.js
+++ sw/sw.js
@@ -8,15 +8,15 @@ self.addEventListener('activate', function(event) {
 });
 self.addEventListener('fetch', function(event) {
     console.log('Fetch event received', event);
-    clients.matchAll({type: 'window'}).then(function (clientList) {
-        console.log('Attempt focus');
-        clientList[0].focus();
-    });
     return fetch(event.request);
 });
 self.addEventListener('notificationclick', function(event) { 
     console.log('Notification click event received', event);
-    clients.openWindow('//cnn.com');
+    clients.matchAll({type: 'window'}).then(function (clientList) {
+        console.log('Attempt focus');
+        clientList[0].focus();
+    });
+    //clients.openWindow('//cnn.com');
 });
-//var me = new Event('notificationclick');
-//self.dispatchEvent(me);
+var me = new Event('notificationclick');
+self.dispatchEvent(me);
